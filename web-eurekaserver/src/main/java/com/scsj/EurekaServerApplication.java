67bfed0f2b3875f361fc96e066f5a6d67c70f518
cxbzsn <1329502472@qq.com>
2018-05-10 17:30:38 +0800

    注册中心服务模块

---
+++ web-eurekaserver/src/main/java/com/scsj/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.scsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+ * @ClassName: EurekaServerApplication 
+ * @Description: EurekaServerApplication EurekaServer服务注册中心
+ * @author caoxb
+ * @date 2018年5月10日 下午2:29:12 
+*/
+@SpringBootApplication
+public class EurekaServerApplication {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+	
+	
+
+}
